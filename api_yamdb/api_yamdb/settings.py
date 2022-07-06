9dbc5b18125f800b43289b205a09a99523a34f60
Александр Белей <beleysasha@gmail.com>
2022-07-06 22:47:30 +1200

    api/static again

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -97,7 +97,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'static/')),
+STATICFILES_DIRS = [(os.path.join(BASE_DIR, '/api/static/')),
                     ('redoc', '/app/templates/')]
 #
 
