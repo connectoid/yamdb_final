208ee027819448e8f6dcdd99cd0a58c6148a9e1b
Александр Белей <beleysasha@gmail.com>
2022-07-07 09:30:58 +1200

    new vm fix

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -99,7 +99,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # STATICFILES_DIRS = [(os.path.join(BASE_DIR, '/api/static/')),
 #                    ('redoc', '/app/templates/')]
-
+#
 # STATICFILES_DIRS = [BASE_DIR + '/api/static/']
 
 AUTH_USER_MODEL = 'reviews.User'
